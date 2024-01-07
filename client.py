f8e98bc4a59525b5d59e4f3a93bc52145fcb9bed
ofir Tsabban <ofirtsabban5@gmail.com>
2024-01-07 20:59:10 +0200

    message forwarding in client

--- client.py
+++ client.py
@@ -4,7 +4,6 @@ import diffieHelmanHelper
 def client_program():
     host = socket.gethostname()  # as both code is running on same pc
     port = 5000  # socket server port number
-
     client_socket = socket.socket()  # instantiate
     client_socket.connect((host, port))  # connect to the server
 
@@ -16,11 +15,39 @@ def client_program():
         if str(data) == "1":
             key = diffie_helman(str(data), client_socket)
             print(key)
+        if str(data) == "2":
+            packet = client_socket.recv(1024).decode()  # receive response
+            data = message_forwarding(packet)
+            client_socket.send(data.encode())  # send message
         message = input(" -> ")
         print('Received from server: ' + message)  # show in terminal
         #server checks if user already has a key.
 
     client_socket.close()  # close the connection
+
+def message_forwarding(packet): 
+    message_recived = False
+    fail = False
+    while message_recived == False:
+        port = 5000
+        #get ip & port from packet
+        server_socket = socket.socket()  # get instance
+        #look closely. The bind() function takes tuple as argument
+        server_socket.bind((host, port))  # bind host address and port together
+        server_socket.listen(1)
+        conn, address = server_socket.accept()  # accept new connection
+        print("Connection from: " + str(address))
+        while True:
+            # receive data stream. it won't accept data packet greater than 1024 bytes
+            data = conn.recv(1024).decode()
+            if not data:
+                # if data is not received break
+                fail = True
+                break
+            message_recived = True
+    if fail == True:
+        data = "couldnt reach the page\n"
+    return data
 
 def diffie_helman(data, socket):
     data = socket.recv(1024).decode()  # receive response
